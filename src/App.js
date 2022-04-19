a6e6a51844ac140e2c491f816f910a320750e184
devstefancho <skku7714@gmail.com>
2022-04-19 18:45:43 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Lyrics } from './components/Lyrics';
+
+function App() {
+  return (
+    <div className="App">
+      <Lyrics />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Lyrics.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import './Lyrics.css';
+import { data } from './data';
+
+export function Lyrics() {
+    const [idx, setIdx] = useState(0)
+    const prevClickHandler = () => {
+        setIdx(prev => prev > 0 ? prev - 1 : prev);
+    }
+    const nextClickHandler = () => {
+        setIdx(prev => prev < data.length - 1 ? prev + 1 : prev);
+    }
+    return (
+        <div className='center'>
+            <div className={`tag`} style={{ backgroundColor: data[idx].color }}>{data[idx].tag}</div>
+            <div onClick={prevClickHandler} className="arrow">&lt;</div>
+            <section className={data[idx].class}>{data[idx].text}</section>
+            <div onClick={nextClickHandler} className="arrow">&gt;</div>
+        </div>
+    )
+}
